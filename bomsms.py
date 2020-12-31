2328752c955669504ee466173ed8de7fb735aa0a
linuxkukeren <46648684+linuxkukeren@users.noreply.github.com>
2020-12-31 16:13:28 +0800

    Update bomsms.py

--- bomsms.py
+++ bomsms.py
@@ -1,6 +1,9 @@
 import requests
 
 no = int(input('masukan no target : '))
+print('===============================================')
+print('jangan lebih dari 10 yah seklai kirim pesan nya \ndi ulang ulang saja \ncontoh : kamu kirim 9 \nterus kamu ulang lagi script nya kirim 9\njangan langsung satu kali kirim lebih dari 10\nbisa kena block nomor targetnya2')
+print('===============================================')
 jum = int(input('masukan jumlah pesan : '))
 
 json = {'phone' :no}
